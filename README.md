243a938b2d88196f1fccd4c71e7dcca1cf548008
sbsajibsx <golamrobbanisajib@gmail.com>
2025-01-24 23:27:13 +0600

    site is ready

--- README.md
+++ README.md
@@ -2,19 +2,77 @@
 
 This site you can buy players like BPL and IPL site.
 
-## Links for visit
+## Project Image
 
-- Github: ( https://github.com/programming-hero-web-course1/b10a7-dream-11-sbsajibsx )
-- Live: ( https://marvelous-pixie-c47098.netlify.app )
+<img src="https://i.ibb.co.com/nbxXgVW/Screenshot-2025-01-24-232241.png" width="800" />
+
+# Main Development features
+
+React JS
+<br>
+Tailwind CSS
+<br>
+React Toastify
+
 
 # 3 key features of my project:
 
 - You can buy players.
 - you can remove player where you select.
 - you can claim some coin.
-# Technologies used: 
-- ES6
-- Condition
-- Tailwind css
-- React Icon
-- React Toastify
+
+# All Development features
+
+## Dependencies
+
+- "prop-types": "^15.8.1",
+- "react": "^18.3.1",
+- "react-dom": "^18.3.1",
+- "react-icons": "^5.3.0",
+- "react-toastify": "^10.0.6"
+
+## Dev Dependencies
+
+- "@eslint/js": "^9.13.0",
+- "@types/react": "^18.3.11",
+- "@types/react-dom": "^18.3.1",
+- "@vitejs/plugin-react": "^4.3.3",
+- "autoprefixer": "^10.4.20",
+- "daisyui": "^4.12.13",
+- "eslint": "^9.13.0",
+- "eslint-plugin-react": "^7.37.1",
+- "eslint-plugin-react-hooks": "^5.0.0",
+- "eslint-plugin-react-refresh": "^0.4.13",
+- "globals": "^15.11.0",
+- "postcss": "^8.4.47",
+- "tailwindcss": "^3.4.14",
+- "vite": "^5.4.9"
+
+# Installation
+
+### gitHub Clone
+
+Copy this command and paste your windows CDM
+
+```
+git clone https://github.com/sbsajibsx/buy-players.git
+
+```
+
+<br>
+
+Type `npm i`
+
+<br>
+
+Add your `.env`
+
+<br>
+
+Then Type `npm run dev`
+
+
+## Links for visit
+
+- Github: ( https://github.com/sbsajibsx/buy-players )
+- Live: ( https://marvelous-pixie-c47098.netlify.app )
--- src/components/Player/Player.jsx
+++ src/components/Player/Player.jsx
@@ -1,18 +1,22 @@
-import PropTypes from 'prop-types';
-import { FaUser } from "react-icons/fa";
-import { FaFlag } from "react-icons/fa";
-import { FaDollarSign } from "react-icons/fa";
+import PropTypes from "prop-types";
+import { FaUser, FaFlag, FaDollarSign } from "react-icons/fa";
 
 const Player = ({ player, handleSelectedPlayers }) => {
-  console.log(Player);
+  console.log(player);
+
   const { name, price, country, hand, img, rating } = player;
+
   return (
-    <div>
-      <div className="card card-compact bg-base-100 shadow-xl">
-        <figure className="p-4">
-          <img className="md:h-96 w-full rounded-2xl" src={img} alt="Shoes" />
-        </figure>
-        <div className="card-body space-y-3">
+    <div className="card card-compact bg-base-100 shadow-xl h-full">
+      <figure>
+        <img
+          className="w-full h-[400px] object-cover rounded-2xl py-6"
+          src={img}
+          alt={name}
+        />
+      </figure>
+      <div className="card-body flex flex-col justify-between space-y-3">
+        <div>
           <h2 className="card-title">
             <FaUser /> {name}
           </h2>
@@ -35,27 +39,31 @@ const Player = ({ player, handleSelectedPlayers }) => {
               <p>Bat Hand</p>
               <p className="text-slate-400 justify-end flex">{hand}</p>
             </div>
-            <div className="flex justify-between items-center">
-              <p className="flex items-center gap-0">
-                Price:
-                <FaDollarSign />
-                {price}
-              </p>
-              <div>
-                <button onClick={()=>handleSelectedPlayers(player)} className="btn">Choose Player</button>
-              </div>
-            </div>
           </div>
+        </div>
+        <div className="flex justify-between items-center">
+          <p className="flex items-center gap-1">
+            Price: <FaDollarSign /> {price}
+          </p>
+          <button onClick={() => handleSelectedPlayers(player)} className="btn">
+            Choose Player
+          </button>
         </div>
       </div>
     </div>
   );
 };
 
-Player.propTypes ={
-  player: PropTypes.object.isRequired,
+Player.propTypes = {
+  player: PropTypes.shape({
+    name: PropTypes.string.isRequired,
+    price: PropTypes.number.isRequired,
+    country: PropTypes.string.isRequired,
+    hand: PropTypes.string.isRequired,
+    img: PropTypes.string.isRequired,
+    rating: PropTypes.number.isRequired,
+  }).isRequired,
   handleSelectedPlayers: PropTypes.func.isRequired,
-  
-}
+};
 
 export default Player;
